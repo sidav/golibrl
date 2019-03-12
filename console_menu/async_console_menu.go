02b4d724baf08fc1c748c866bd3742f64b96be3c
Vladislav Kovun <vkovun@phoenixit.ru>
2019-03-12 23:13:01 +0300

    Adds an async ("passive") menu.

---
+++ console_menu/async_console_menu.go
@@ -0,0 +1,39 @@
+package console_menu
+
+import (
+	cw "github.com/sidav/goLibRL/console"
+	"strings"
+)
+
+// returns index of element. Is not looped, does not flush the console and is purposed for alongside-usage.
+func DrawSidebarMouseOnlyAsyncMenu(title string, titleColor, mx, my, mw int, items []string) int{
+	drawSidebarMenuTitle(title, titleColor, mx, my, mw)
+	mh := len(items)+1 // +1 is for the title
+
+	cursorIndex := -1
+	mousex, mousey := cw.GetMouseCoords()
+	if mousex >= mx && mousex < mx+mw && mousey > my && mousey < my + mh {
+		cursorIndex = mousey - my - 1
+	}
+
+	for y := 1; y < mh; y++ {
+		cw.PutString(strings.Repeat(" ", mw), mx, y+my) // clear menu screen space
+	}
+	for i := 0; i < len(items); i++ {
+		str := items[i]
+		if i == cursorIndex {
+			cw.SetBgColor(cw.BEIGE)
+			cw.SetFgColor(cw.BLACK)
+			// str = "->"+str
+		} else {
+			cw.SetBgColor(cw.BLACK)
+			cw.SetFgColor(cw.BEIGE)
+		}
+		// str += strings.Repeat(" ", mw - len(str)) // fill the whole menu width
+		cw.PutString(str, mx, my+i+1)
+		cw.SetBgColor(cw.BLACK)
+		cw.SetFgColor(cw.BEIGE)
+	}
+	cw.SetBgColor(cw.BLACK)
+	return cursorIndex
+}
