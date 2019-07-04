7cbfabe68f8ca722391586e6d114c0aeadf7c6a6
Vladislav Kovun <vkovun@phoenixit.ru>
2019-07-04 18:00:34 +0300

    Adds the cellular automata cave generator.

--- stub.go
+++ stub.go
@@ -1,1 +1,21 @@
-package golibrl
+package main
+
+import (
+	"fmt"
+	"github.com/sidav/golibrl/procedural_generation"
+)
+
+func main() {
+	cave := procedural_generation.MakeCave(60, 20, 3, -1)
+	for _, s := range *cave {
+		fmt.Println(s)
+	}
+	//console.Init_console("test", console.TCellRenderer)
+	//defer console.Close_console()
+	//console.SetFgColor(console.WHITE)
+	//for y, s := range *cave {
+	//	console.PutString(s, 0, y)
+	//}
+	//console.Flush_console()
+	//console.ReadKey()
+}
