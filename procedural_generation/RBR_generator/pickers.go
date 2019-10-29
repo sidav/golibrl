46bea9d7c4621243c92149fe7925037ff81d2f0e
Vladislav Kovun <vkovun@phoenixit.ru>
2019-10-29 13:41:29 +0300

    Adds isTileSutableForJunction()

--- procedural_generation/RBR_generator/pickers.go
+++ procedural_generation/RBR_generator/pickers.go
@@ -17,22 +17,31 @@ func (r *RBR) pickListOfDiggableDirectionsFrom(x, y int, allowContinuation bool)
 	return &dirs
 }
 
+func (r *RBR) isTileSuitableForJunction(x, y int, deadendOnly bool) bool {
+	if r.tiles[x][y].tiletype == TWALL {
+		if deadendOnly {
+			walls := r.countTiletypesAround(TWALL, x, y, true)
+			floors := r.countTiletypesAround(TFLOOR, x, y, false)
+			if walls == 7 && floors == 1 {
+				return true
+			}
+		} else {
+			walls := r.countTiletypesAround(TWALL, x, y, false)
+			floors := r.countTiletypesAround(TFLOOR, x, y, false)
+			if walls == 3 && floors == 1 {
+				return true
+			}
+		}
+	}
+	return false
+}
+
 func (r *RBR) pickJunctionTile(fromx, fromy, tox, toy int, deadendOnly bool) (int, int) {
 	listOfAppropriateCoords := make([][]int, 0)
 	for x := fromx; x < tox; x++ {
 		for y := fromy; y < toy; y++ {
-			if deadendOnly {
-				walls := r.countTiletypesAround(TWALL, x, y, true)
-				floors := r.countTiletypesAround(TFLOOR, x, y, false)
-				if r.tiles[x][y].tiletype == TWALL && walls == 7 && floors == 1 {
-					listOfAppropriateCoords = append(listOfAppropriateCoords, []int{x, y})
-				}
-			} else {
-				walls := r.countTiletypesAround(TWALL, x, y, false)
-				floors := r.countTiletypesAround(TFLOOR, x, y, false)
-				if r.tiles[x][y].tiletype == TWALL && walls == 3 && floors == 1 {
-					listOfAppropriateCoords = append(listOfAppropriateCoords, []int{x, y})
-				}
+			if r.isTileSuitableForJunction(x, y, deadendOnly) {
+				listOfAppropriateCoords = append(listOfAppropriateCoords, []int{x, y})
 			}
 		}
 	}
@@ -42,3 +51,21 @@ func (r *RBR) pickJunctionTile(fromx, fromy, tox, toy int, deadendOnly bool) (in
 	indx := rnd.Rand(len(listOfAppropriateCoords))
 	return listOfAppropriateCoords[indx][0], listOfAppropriateCoords[indx][1]
 }
+
+// func (r *RBR) pickJunctionTileForRoom(rx, ry, w, h int) (int, int) {
+// 	listOfAppropriateCoords := make([][]int, 0)
+// 	for x := rx - 1; x <= rx+w; x++ {
+// 		for y := ry - 1; y <= ry+h; y++ {
+// 			if (x == rx-1 || x == rx+w) && (y == ry-1 || y == ry+h) {
+// 				if r.isTileSuitableForJunction(x, y, false) {
+// 					listOfAppropriateCoords = append(listOfAppropriateCoords, []int{x,y})
+// 				}
+// 			}
+// 		}
+// 	}
+// 	if len(listOfAppropriateCoords) == 0 {
+// 		return -1, -1 
+// 	}
+// 	coord := listOfAppropriateCoords[rnd.Rand(len(listOfAppropriateCoords))] 
+// 	return coord[0], coord[1]
+// }
