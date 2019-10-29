fccf4cd9f96c9d297775233fa057c0197193aea4
Vladislav Kovun <vkovun@phoenixit.ru>
2019-10-29 16:55:48 +0300

    Adds one more initial layout.

--- procedural_generation/RBR_generator/initial_layout.go
+++ procedural_generation/RBR_generator/initial_layout.go
@@ -2,8 +2,8 @@ package RBR_generator
 
 // returns number of rooms and corridors placed.
 func (r *RBR) placeInitialLayout() (int, int) {
-	layout := rnd.RandInRange(0, 2)
-	layout = 1
+	layout := rnd.RandInRange(0, 3)
+	
 	rooms, corrs := 0, 0
 	switch layout {
 	case 0:
@@ -11,6 +11,8 @@ func (r *RBR) placeInitialLayout() (int, int) {
 		rooms, corrs = r.placeInitialCorridorRings(corridorRings)
 	case 1:
 		rooms, corrs = r.placeInitialTwoInterconnectedRooms()
+	case 2:
+		rooms, corrs = r.placeInitialFourInterconnectedRooms()
 	default:
 		rooms, corrs = r.placeInitialLargeRoom()
 	}
@@ -34,7 +36,7 @@ func (r *RBR) placeInitialLargeRoom() (int, int) {
 func (r *RBR) placeInitialTwoInterconnectedRooms() (int, int) {
 	w1 := rnd.RandInRange(r.MAX_RSIZE/2, r.MAX_RSIZE)
 	w2 := w1 // rnd.RandInRange(r.MIN_RSIZE, r.MAX_RSIZE)
-	x1 := rnd.RandInRange(1, r.mapw/2-w1)
+	x1 := rnd.RandInRange(1, r.mapw/3)
 	x2 := r.mapw - x1 - w2 // rnd.RandInRange(r.mapw/2, r.mapw-w2)
 	h := rnd.RandInRange(r.MAX_RSIZE/2, r.MAX_RSIZE)
 	y := rnd.RandInRange(r.maph/4, 3*r.maph/4-h)
@@ -46,9 +48,35 @@ func (r *RBR) placeInitialTwoInterconnectedRooms() (int, int) {
 	}
 	for i := 0; i < corridors; i++ {
 		corrY := rnd.RandInRange(y, y+h-1)
-		r.digSpace(x1+w1, corrY, x2-x1-w1, 1, 1)
+		r.digSpace(x1+w1, corrY, x2-x1-w1, 1, 0)
 	}
 	return 2, corridors
+}
+
+func (r *RBR) placeInitialFourInterconnectedRooms() (int, int) {
+	w := rnd.RandInRange(r.MAX_RSIZE/2, r.MAX_RSIZE)
+	x1 := rnd.RandInRange(1, r.mapw/3)
+	x2 := r.mapw - x1 - w 
+	h := rnd.RandInRange(r.MAX_RSIZE/2, r.MAX_RSIZE)
+	y1 := rnd.RandInRange(1, r.maph/3)
+	y2 := r.maph - y1 - h 
+	r.digSpace(x1, y1, w, h, 1)
+	r.digSpace(x2, y1, w, h, 2)
+	r.digSpace(x1, y2, w, h, 3)
+	r.digSpace(x2, y2, w, h, 4)
+	corridors := rnd.RandInRange(1, h/3)
+	if corridors < 1 {
+		corridors = 1
+	}
+	for i := 0; i < corridors; i++ {
+		corrY := rnd.RandInRange(y1, y1+h-1)
+		r.digSpace(x1+w, corrY, x2-x1-w, 1, 0)
+		r.digSpace(x1+w, r.maph-corrY-1, x2-x1-w, 1, 0)
+		corrX := rnd.RandInRange(x1, x1+w-1)
+		r.digSpace(corrX, y1+h, 1, y2-y1-h, 0)
+		r.digSpace(r.mapw-corrX, y1+h, 1, y2-y1-h, 0)
+	}
+	return 4, 4 * corridors
 }
 
 func (r *RBR) placeInitialCorridorRings(number int) (int, int) {
